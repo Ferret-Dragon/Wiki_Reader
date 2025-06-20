765d54004bdc135f5cf19c388120433a530fe1cc
ferretdragon <hurria.ashman@hotmail.com>
2025-06-20 15:13:30 -0400

    added to do

--- playwright_practice.py
+++ playwright_practice.py
@@ -1,42 +1,5 @@
 from playwright.sync_api import sync_playwright
-
-def run():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)  # Launch headless browser
-        page = browser.new_page()
-
-        #page.goto("https://richmond.craigslist.org/search/acc#search=2~thumb~0")
-        #page.goto("https://richmond.craigslist.org/search/richmond,-va-va/acc?lat=37.5383&lon=-77.4615&search_distance=60#search=2~thumb~0", wait_until="networkidle")
-        page.goto("https://richmond.craigslist.org/search/richmond-va/sof?lat=37.551&lon=-77.459&search_distance=25#search=2~thumb~0", wait_until="networkidle")  # Wait until page is fully loaded
-
-
-
-        html = page.content()  # Get rendered HTML
-        with open("seed_page_content.html", "w", encoding="utf-8") as f:
-            f.write(html)
-        
-        matchingSearchResultsOnPage = page.query_selector_all("div.cl-search-result.cl-search-view-mode-thumb")
-        print(len(matchingSearchResultsOnPage))
-
-        # with open("links_page.html", "w", encoding="utf-8") as f:
-        #     for link in items:
-        #         f.write(str(link))
-        firstResult = matchingSearchResultsOnPage[0]
-        print(type(firstResult))
-        print(dir(type(firstResult)))
-        print("-----------")
-        print("inner html of element : ",firstResult.inner_html())
-        print("Title of element : ", firstResult.get_attribute("title"))
-
-
-        link_el = firstResult.query_selector("a")
-        if link_el:
-            print("Link in result : ", link_el.get_attribute("href"))
-            print("inner text of link : ", link_el.inner_text())
-            # for data in firstResult:
-        #     print(data)
-        
-        browser.close()
+import sqlite3
 
         
 def visit_seed_web_page(page_to_visit, browser):
@@ -72,11 +35,24 @@ def get_web_address_for_link_element(playwrightElementHandle):
 
     return "none listed"
     
-    
-    
 if __name__ == "__main__":
 
     link="https://richmond.craigslist.org/search/richmond-va/sof?lat=37.551&lon=-77.459&search_distance=25#search=2~thumb~0"
+
+    ### Open connection to the database
+    conn = sqlite3.connect("craigslist_jobs.db")
+    cur = conn.cursor()
+
+    ### Create Database if it does not exist
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS jobs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            job TEXT,
+            url TEXT
+        )
+    """)
+    conn.commit()
+
 
     with sync_playwright() as p:
         #### Initialize our Browser
@@ -96,10 +72,18 @@ if __name__ == "__main__":
             webAddress = get_web_address_for_link_element(data)
             print("title ", title)
             print("web address ", webAddress, "\n")
-        
+            
+            cur.execute("INSERT INTO jobs (job, url) VALUES (?, ?)", (title, webAddress))
+
+        conn.commit()
+        conn.close()
         browser.close()
 
-        
+        # TODO - 
+        # Visit each of these links
+        # Determine what data is important / related to the job listing (i.e. parse data for critical information)
+        # Send to an LLM to ask for a 1 sentence summary of the job.
+        # Store that result in our database.
         
 
 
